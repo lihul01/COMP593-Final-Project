bb9e1b99e3c52aaaa9fa2af1eb7cfb662577f3e3
lihul01 <lihul@flemingcollege.ca>
2023-08-07 17:50:55 -0400

    Almost Done

--- COMP593-FinalProject-Partial Solution/image_lib.py
+++ COMP593-FinalProject-Partial Solution/image_lib.py
@@ -5,8 +5,6 @@ import requests
 import ctypes
 
 def main():
-    image_data = download_image('https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg')
-    result = save_image_file(image_data, r'C:\temp\kitty.jpg')
     return
 
 def download_image(image_url):
